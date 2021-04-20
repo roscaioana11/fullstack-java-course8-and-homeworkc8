de10c49697f66010acf55fc5ca39eb06f6f1b9f4
Ioana Rosca <64215525+roscaioana11@users.noreply.github.com>
2021-04-20 17:02:07 +0300

    Add files via upload

---
+++ src/main/java/ro/fasttrackit/fullstackjavacourse8andhomeworkc8/FullstackJavaCourse8AndHomeworkc8Application.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit.fullstackjavacourse8andhomeworkc8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackJavaCourse8AndHomeworkc8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackJavaCourse8AndHomeworkc8Application.class, args);
+	}
+
+}
---
+++ src/test/java/ro/fasttrackit/fullstackjavacourse8andhomeworkc8/FullstackJavaCourse8AndHomeworkc8ApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit.fullstackjavacourse8andhomeworkc8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackJavaCourse8AndHomeworkc8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
