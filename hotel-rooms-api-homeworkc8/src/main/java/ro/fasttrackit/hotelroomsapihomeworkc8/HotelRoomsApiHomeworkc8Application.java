9377f5c7ac1ee1030e5a88f046be821983b71d88
DESKTOP-HSJGVQ7\Ioana <rosca.ioana11@gmail.com>
2021-04-22 16:37:41 +0300

    Homework Course 8 Setup - Hotel Rooms API

---
+++ hotel-rooms-api-homeworkc8/src/main/java/ro/fasttrackit/hotelroomsapihomeworkc8/HotelRoomsApiHomeworkc8Application.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit.hotelroomsapihomeworkc8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelRoomsApiHomeworkc8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelRoomsApiHomeworkc8Application.class, args);
+	}
+
+}
